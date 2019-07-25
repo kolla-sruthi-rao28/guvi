3d7dbbbfd30acce8b133711b9c04e089d1cee6e7
kolla-sruthi-rao28 <51354925+kolla-sruthi-rao28@users.noreply.github.com>
2019-07-25 16:34:12 +0530

    Create rao47.py

---
+++ rao47.py
@@ -0,0 +1,16 @@
+n11=int(input())
+s=0
+for i in range(2,n11):
+    flag=1
+    for j  in range(2,i):
+        if(i%j==0):
+            flag=0
+            break
+    if(flag):
+        k=str(i)
+        l=[]
+        for m in range(len(k)):
+            l.append(k[m])
+        if(l[-1]=='3'):
+            s=s+i
+print(s)       
